0dc67c85b3b75c6408d0f73bea3c183c02e18c18
ayuopy <ayyuopy@protonmail.ch>
2017-07-20 16:41:58 +0100

    added /score endpoint and npm/curl reference

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 api/database services - purely for reference
+
+Run server: `npm start`<br>
+Run test: `npm test`
+
+|type |endpoint |command
+|-----|---------|-----------------
+|GET  |/token:  |`curl http://localhost:8000/token/:name`
+|GET  |/score:  |`curl -G http://localhost:8000/score --data-urlencode "home=home team" --data-urlencode "away=away team"`
+|POST |/add		|`curl http://localhost:8000/add -H 'Content-Type: application/json' -d '{"token": <token>, "home": <home team>, "away": <away team>, "hscore": <home score>, "ascore": <away score>}'`
--- node/server.js
+++ node/server.js
@@ -3,9 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 
+
+// SETUP 
 const app = express();
 const host = '0.0.0.0';
 const port = 8000
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 function newPool() {
 	const pool = new Pool({database: "crud"});
@@ -13,6 +18,8 @@ function newPool() {
 	return pool
 }
 
+
+// TOKEN GENERATION
 app.get('/token/:name', (req, res) => {
 	const name = req.params.name;
 	checkUser(name, (token) => {
@@ -38,7 +45,31 @@ function newToken(name) {
 	return token;
 }
 
-app.use(bodyParser.json());
+
+// CRUD
+app.get('/score', (req, res) => {
+	// no need for token - find score given home and away team parameters
+	const data = req.query;
+	findScore(data, (score) => {
+		res.send(score);
+	});
+})
+
+function findScore(data, callback) {
+	const values = [data.home, data.away];
+	console.log(values);
+	const pool = newPool();
+	pool.query('SELECT hscore, ascore FROM scores WHERE home = $1 AND away = $2', values,
+		(err, res) => {
+			if (res.rowCount > 0) {
+				const result = res.rows[0];
+				callback(`${result.hscore}-${result.ascore}`);
+			} else {
+				callback('Match not found');
+			}
+			pool.end();
+		})
+}
 
 app.post('/add', (req, res) => {
 	const data = req.body;
